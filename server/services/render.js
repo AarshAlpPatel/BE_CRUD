7a89a3094c92ce9a9918a28c72e69b49835d00de
AarshAlpPatel <aarsh.patel@gmail.com>
2022-05-22 18:19:16 -0400

    GET API and Frontend Synced

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,17 @@
+const axios = require('axios');
+
 exports.homeRoutes = (req, res) => {
-    res.render('index');
+    //Make a GET request to the api
+    axios.get("http://localhost:3000/api/users")
+    .then(function(response){
+        console.log(response.data)
+        res.render('index', { items: response.data });
+    })
+    .catch(err => {
+        res.send(err);
+    })
+
+    
 }
 
 exports.add_user = (req, res) => {
