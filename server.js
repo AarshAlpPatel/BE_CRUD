897a3a8d733ab15472ef2435ae6426f25c64499a
AarshAlpPatel <aarsh.patel@gmail.com>
2022-05-22 22:33:22 -0400

    fix cors issue

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path');
+var cors = require('cors')
 
 const connectDB = require('./server/database/connection');
 
 const app = express()
+
+app.use(cors());
+app.options('*', cors());
 
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080
